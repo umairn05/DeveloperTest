19356467403b35307f074111e5a5753e9c1390ae
Umair Naeem <eyes8020@gmail.com>
2019-09-22 15:31:54 +0100

    Unit test without concrete   classes

---
+++ Interview/Interview.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Interview.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Interview.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c01c0536-5223-4eb6-82ae-850ab3bf5088")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Interview/Interview.Test/RepositoryTest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Interview.Test
+{
+    [TestClass]
+    public class RepositoryTest
+    {
+        private IRepository<IStoreable<Object>,Object> repository;
+        private IStoreable<Object> storeable;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            storeable = new Storable<Object>() { Id = 1 };
+            repository = new Repository<IStoreable<Object>,Object>(storeable);
+
+        }
+
+        [TestMethod]
+        [TestCategory("Success")]
+        public void GivenRepository_WhenRequestedForAll_ShouldReturnRepositoryList()
+        {
+            //Arrange 
+            IEnumerable<IStoreable<Object>> result;
+
+            //Act
+            result = repository.GetAll();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(IEnumerable<IStoreable<Object>>));
+            Assert.IsTrue(result.Count() > 0);
+        }
+
+        [TestMethod]
+        [TestCategory("Success")]
+        public void GivenRespository_WhenRequestedForSingleRecordById_ShouldReturnSingleReocrdFromRepository()
+        {
+
+            //Arrange
+            int intId = 1;
+
+            //Act
+            var result = repository.Get(intId);
+
+            //Assert 
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Id, intId);
+        }
+
+        [TestMethod]
+        [TestCategory("Success")]
+        public void GivenRepository_WhenReqeustToAddItem_ShouldSaveRecordInRepository()
+        {
+            //Arrange 
+            int intId = 1;
+            var newRecordInTypeId= new Storable<Object> { Id = intId };
+   
+            //Act 
+            repository.Save(newRecordInTypeId);
+            var result = repository.Get(intId);
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Id, intId);
+
+        }
+
+        [TestMethod]
+        [TestCategory("Success")]
+        public void GivenRepository_WhenRequestToRemoveItem_ShouldDeleteRecordFromRepository()
+        {
+            //Arrange 
+            int intId = 1;
+
+            //Act
+            repository.Delete(intId);
+            IStoreable<Object> result = repository.Get(intId);
+
+            //Assert
+            Assert.IsNull(result);
+        }
+    }
+}
